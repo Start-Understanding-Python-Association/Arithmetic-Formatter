754c27aecab769f03c0fd435cfef5b6d84cc3b32
Daniel Barthelmeh <dcbarthelmeh@gmail.com>
2020-09-06 21:05:27 -0600

    Added a few comments and got rid of the extra spaces on each line.

--- arithmetic_arranger.py
+++ arithmetic_arranger.py
@@ -5,8 +5,8 @@ def arithmetic_arranger(problems, show_answers=False):
      we can show answers."""
     if len(problems) > 5:
         print('Error: Too many problems.')
-    for i in problems:
-        if '*' in i or '/' in i:
+    for problem in problems:
+        if '*' in problem or '/' in problem:
             print('Error: Operator must be "+" or "-".')
             break
     # Error: Numbers must only contain digits.
@@ -15,27 +15,26 @@ def arithmetic_arranger(problems, show_answers=False):
     line_2 = ''
     line_3 = ''
     line_4 = ''
-    for i in problems:
+    for problem in problems:
         number_one = ''
         number_two = ''
         sign = ''
         # answer = ''
         switch_to_second_number = False
 
-        for j in i:
-            # line_2, line_3, line_4
-            if j != ' ' and j != '+' and j != '-' and not switch_to_second_number:
-                number_one = number_one + j
+        for character in problem:
+            if character != ' ' and character != '+' and character != '-' and not switch_to_second_number:
+                number_one = number_one + character
                 continue
-            elif j == '+' or j == '-':
+            elif character == '+' or character == '-':
                 switch_to_second_number = True
-                sign = j
+                sign = character
                 continue
-            elif j != ' ' and switch_to_second_number:
-                number_two = number_two + j
+            elif character != ' ' and switch_to_second_number:
+                number_two = number_two + character
                 continue
 
-        if len(number_one) > len(number_two):
+        if len(number_one) > len(number_two):  # take the greater of the lengths of the two numbers and add 2
             width_of_problem = len(number_one) + 2
         else:
             width_of_problem = len(number_two) + 2
@@ -44,7 +43,12 @@ def arithmetic_arranger(problems, show_answers=False):
         line_3 = line_3 + width_of_problem * '-' + '    '
         # if show_answers:
         #     answer = int(number_one) + int(number_two)
-        #     line_4 = line_4 + (width_of_problem - len)
-    arranged_problems = (line_1, '\n', line_2, '\n', line_3, '\n', line_4)
-
+        #     line_4 = line_4 + (width_of_problem - len
+    line_1 = line_1[:-4]  # take back the 4 extra spaces at the end of each line
+    line_2 = line_2[:-4]
+    line_3 = line_3[:-4]
+    # line_4 = line_4[:-4])
+    arranged_problems = line_1 + '\n' + line_2 + '\n' + line_3
+    if show_answers:
+        arranged_problems = arranged_problems + '\n' + line_4
     return arranged_problems
--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 # This entrypoint file to be used in development. Start by reading README.md
 from arithmetic_arranger import arithmetic_arranger
-from unittest import m
+from unittest import main
 
 
 print(arithmetic_arranger(["32 + 698", "3801 - 2", "45 + 43", "123 + 49"]))
